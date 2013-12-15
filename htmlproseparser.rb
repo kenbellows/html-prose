27a41076fb196151ea324823e96aa453455a4f6c
Ken Bellows <ken.bellows@live.com>
2013-12-15 08:54:55 -0500

    Added basic support for children and properties. nothing's been tested yet, though. that's next.

--- htmlproseparser.rb
+++ htmlproseparser.rb
@@ -6,15 +6,20 @@ require 'phtmlnodes.rb'
 # Find out what our base path is
 pwd = File.expand_path(File.dirname(__FILE__))
 
-class Parser
+class HTMLProseParser
   # Load the Treetop grammar from the 'phtml.treetop' file and
   # create a new instance of that parser as a class variable
   Treetop.load 'phtml.treetop'
   @@parser = HtmlProseParser.new
   
   def self.parse(data)
-    # Pass the data over to the parser instance
-    tree = @@parser.parse(data)
+    if data.strip == ''
+      tree - ''
+    else
+      # Pass the data over to the parser instance
+      tree = @@parser.parse(data)
+    end
+    
      
     # If the AST is nil then there was an error during parsing
     # we need to report a simple error message to help the user
@@ -26,8 +31,20 @@ class Parser
   end
   
   def self.parse_file(filename)
+    result = []
     File.open(filename) do |file|
-      self.parse(file.read)
+      file.readlines.each do |line|
+        unless line.strip == ''
+          result << self.parse(line)
+        end
     end
+    return result
+  end
+end
+
+
+if $0 == __FILE__
+  STDIN.readlines.each do |line|
+    puts Parser.parse line
   end
 end
